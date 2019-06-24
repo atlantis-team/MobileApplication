ed31391b3ab5bf065a2f31d620d9bac799f5fc22
Raphael O <raphael.octau@yahoo.fr>
2019-06-24 15:53:55 +0200

    Added table with fake data in data page

--- AtlantisApp/src/pages/device-information/device-information.ts
+++ AtlantisApp/src/pages/device-information/device-information.ts
@@ -18,6 +18,59 @@ export class DeviceInformationPage {
 
   deviceId: number;
 
+  public rawData = [
+    {
+      id: 0,
+      value: 646531,
+      type: "lol"
+    },
+    {
+      id: 1,
+      value: 97846513,
+      type: "lol"
+    },
+    {
+      id: 2,
+      value: 849651,
+      type: "lol"
+    },
+    {
+      id: 3,
+      value: 2164,
+      type: "lol"
+    },
+    {
+      id: 4,
+      value: 4651,
+      type: "lol"
+    },
+    {
+      id: 5,
+      value: 789,
+      type: "lol"
+    },
+    {
+      id: 6,
+      value: 1561,
+      type: "lol"
+    },
+    {
+      id: 7,
+      value: 6432,
+      type: "lol"
+    },
+    {
+      id: 8,
+      value: 964823,
+      type: "lol"
+    },
+    {
+      id: 9,
+      value: 9865,
+      type: "lol"
+    }
+  ];
+
   constructor(public appCtrl: App, public navParams: NavParams) {
     this.deviceId = navParams.get('id');
   }
